8fe75f9473b26bbfffcab99496be785d0c143e64
SahilTallnia <sahiltallnia222@gmail.com>
2022-10-07 22:05:51 +0530

    dashboard backend

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,9 @@ app_name='accounts'
 urlpatterns = [
     path('register',views.registerUser,name='registerUser'),
     path('register-vendor',views.registerVendor,name='registerVendor'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('myAccount',views.myAccount,name='myAccount'),
+    path('custDashboard',views.custDashboard,name='custDashboard'),
+    path('vendorDashboard',views.vendorDashboard,name='vendorDashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render,redirect
-from django.contrib import messages
-
+from django.contrib import messages,auth
+from .utils import detectUser
+from django.contrib.auth.decorators import login_required,user_passes_test
 from vendor.vendorForm import VendorForm
 from .form import UserForm
-# Create your views here.
+from django.core.exceptions import PermissionDenied
 from .models import User, UserProfile
+
+
+def check_vendor_role(user):
+    if user.role==1:
+        return True
+    else:
+        raise PermissionDenied
+
+def check_cust_role(user):
+    if user.role==2:
+        return True
+    else:
+        raise PermissionDenied
+
 def registerUser(request):
     
-    if(request.method=='POST'):
+    if request.user.is_authenticated :
+        messages.warning(request,'You are already logged in.')
+        return redirect('accounts:myAccount')
+    elif(request.method=='POST'):
         form=UserForm(request.POST)
         if form.is_valid():
             # commit=false means taht assign the values to the user but not save the user
@@ -39,8 +57,10 @@ def registerUser(request):
 
 
 def registerVendor(request):
-    
-    if request.method=='POST':
+    if request.user.is_authenticated :
+        messages.warning(request,'You are already logged in.')
+        return redirect('accounts:myAccount')
+    elif request.method=='POST':
         form=UserForm(request.POST)
         v_form=VendorForm(request.POST,request.FILES)
         
@@ -75,3 +95,49 @@ def registerVendor(request):
 
 
 # messages, we don't need to pass in to the form to access in to any html file because it is passed in to the context_processor which makes it available to all the html files to check open settings templates there is something called context_processor which messages module in written.
+
+
+def login(request):
+    if request.user.is_authenticated :
+        messages.warning(request,'You are already logged in.')
+        return redirect('accounts:myAccount')
+    elif request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        print(email)
+        print(password)
+        user=auth.authenticate(email=email,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,'You are logged in.')
+            return redirect('accounts:myAccount')
+        else:
+            messages.error(request,'Invalid credentials')
+            return redirect('accounts:login')
+    
+    return render(request,'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request,'You are logged out.')
+    return redirect('accounts:login')
+
+@login_required(login_url='accounts:login')
+def myAccount(request):
+    user=request.user
+    redirectUrl=detectUser(user)
+    return redirect(redirectUrl)
+
+
+@login_required(login_url='accounts:login')
+@user_passes_test(check_cust_role)
+def custDashboard(request):
+    return render(request,'accounts/custDashboard.html')
+
+
+
+@login_required(login_url='accounts:login')
+@user_passes_test(check_vendor_role)
+def vendorDashboard(request):
+    return render(request,'accounts/vendorDashboard.html')
